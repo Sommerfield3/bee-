4ec7b2ebf63eb8d11ded692308193ce38eb9d7f5
Linghai Joaquin, Zhong Callasi <104087488+Sommerfield3@users.noreply.github.com>
2025-02-20 13:02:32 -0500

    Create 1016-Distance.cpp

---
+++ 1016-Distance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	/**
+	* Escreva a sua soluç ão aqui
+	* Code your solution here
+	* Escriba su solución aquí
+	*/
+	int dist;
+	cin>>dist;
+	dist=dist*2;
+	cout<<dist<<" minutos"<<endl;
+	return 0;
+}
