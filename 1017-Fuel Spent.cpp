827568fd3ff39fd8a40111e8814d757fd6eae69a
Linghai Joaquin, Zhong Callasi <104087488+Sommerfield3@users.noreply.github.com>
2025-02-20 14:14:05 -0500

    Create 1017-Fuel Spent.cpp

---
+++ 1017-Fuel Spent.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	
+	/**
+	* Escreva a sua solução aqui
+	* Code your solution here
+	* Escriba su solución aquí
+	*/
+	double t,s;
+	cin>>t>>s;
+	double total=(t*s)/12.0;
+	cout<<fixed<<setprecision(3)<<total<<endl;
+	return 0;
+}
