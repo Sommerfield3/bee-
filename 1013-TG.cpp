31211d3453f33a91a90f7648cd679c2fd7a1e9e1
Linghai Joaquin, Zhong Callasi <104087488+Sommerfield3@users.noreply.github.com>
2025-02-19 22:47:09 -0500

    Update 1013-TG.cpp

--- 1013-TG.cpp
+++ 1013-TG.cpp
@@ -1,3 +1,8 @@
+/*
+Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+MaiorAB=a+b+abs(a-b)/2
+*/
+
 #include <stdio.h>
 #include <cmath>
 using namespace std; 
